9e28185af593c445604e3d345a688f485fb8f5e1
agustinesco <agustinesco@192.168.0.14>
2021-05-31 07:45:32 -0300

    agregado variable fechaVencimiento con metodo estaVencida()

--- src/centroVacunacion/Moderna.java
+++ src/centroVacunacion/Moderna.java
@@ -1,16 +1,34 @@
 package centroVacunacion;
 
+
 public class Moderna extends Vacuna {
-	private Fecha fechaVencimiento;
 	
-	Moderna(String edad, int temperatura, Fecha fechaVencimiento) {
-		super(edad, temperatura);
-		this.fechaVencimiento = fechaVencimiento;
+	private Fecha fechaVencimiento;
+	private Fecha aux;
+	
+	
+	Moderna(Fecha fechaIngreso) {
+		super("todas las edades", -18);
+		
+		//variable auxiliar para establecer la fecha de vencimiento
+		aux =new Fecha(fechaIngreso.dia() , fechaIngreso.mes() , fechaIngreso.anio());
+		
+		//avanzamos los dias necesarios para que se venza 
+		for (int i = 0; i < 60; i++) {
+			aux.avanzarUnDia();
+		}
+		this.fechaVencimiento = aux;
 	}
 	
 	
 	//getters y setters
 	public Fecha getFechaVencimiento() {
+		StringBuilder fecha = new StringBuilder();
+		fecha.append(fechaVencimiento.dia());
+		fecha.append("/");
+		fecha.append(fechaVencimiento.mes());
+		fecha.append("/");
+		fecha.append(fechaVencimiento.anio());
 		return fechaVencimiento;
 	}
 
@@ -18,6 +36,14 @@ public class Moderna extends Vacuna {
 		this.fechaVencimiento = fechaVencimiento;
 	}
 	
-	
-
+	@Override
+	public boolean estaVencida() {
+		if(Fecha.hoy().compareTo(fechaVencimiento) >=0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
+
--- src/centroVacunacion/Pfizer.java
+++ src/centroVacunacion/Pfizer.java
@@ -2,10 +2,17 @@ package centroVacunacion;
 
 public class Pfizer extends Vacuna {
 	private Fecha fechaVencimiento;
+	private Fecha aux;
 
-	Pfizer(String edad, int temperatura, Fecha fechaVencimiento) {
-		super(edad, temperatura);
-		this.fechaVencimiento = fechaVencimiento;
+	Pfizer(Fecha fechaIngreso) {
+		super("mayores de 60", -18);
+
+		aux = new Fecha(fechaIngreso.dia(), fechaIngreso.mes(), fechaIngreso.anio());
+
+		for (int i = 0; i < 30; i++) {
+			aux.avanzarUnDia();
+		}
+		this.fechaVencimiento = aux;
 	}
 
 	public Fecha getFechaVencimiento() {
@@ -15,7 +22,14 @@ public class Pfizer extends Vacuna {
 	public void setFechaVencimiento(Fecha fechaVencimiento) {
 		this.fechaVencimiento = fechaVencimiento;
 	}
-	
-	
 
+	@Override
+	public boolean estaVencida() {
+		if (Fecha.hoy().compareTo(fechaVencimiento) >= 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 }
