c975ea520e22c0756dfdbd107e3fd32eacd7278a
agustinesco <agustinesco@192.168.0.14>
2021-05-31 23:07:43 -0300

    muchos metodos modificados

--- src/centroVacunacion/CentroVacunacion.java
+++ src/centroVacunacion/CentroVacunacion.java
@@ -1,14 +1,21 @@
 package centroVacunacion;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
 public class CentroVacunacion {
 
-	//valores
+	//valores del constructor
 		private String nombreDelCentro;
 		private int capacidadVacunacionDiaria;
-		private Almacen almacen = new Almacen();
+		private Almacen almacen;
+		HashMap<Integer, String> reporteVacunacion = new HashMap<>();
+		ArrayList<Persona> listaDeEspera = new ArrayList<>();
+		HashMap<Fecha, ArrayList<Turno>> turnos = new HashMap<>();
+	//valores auxiliares
+		
 	/*
 	 * IREP
 	 * la capacidadVacunacionDiaria debe ser un entero mayor a 0
@@ -23,7 +30,11 @@ public class CentroVacunacion {
 	* Si el nombre no está definido, se debe generar una excepción.
 	*/
 	public CentroVacunacion(String nombreCentro, int capacidadVacunacionDiaria) {
-		
+		if(nombreCentro.startsWith(" ")) throw new RuntimeException("El nombre no está definido");
+		this.nombreDelCentro = nombreCentro;
+		if(capacidadVacunacionDiaria <=0) throw new RuntimeException("La vacunacion diaria no puede ser menor a 1");
+		this.capacidadVacunacionDiaria = capacidadVacunacionDiaria;
+		this.almacen = new Almacen();
 	}
 	
 	
@@ -37,17 +48,22 @@ public class CentroVacunacion {
 	* sumar al stock existente, tomando en cuenta las vacunas ya utilizadas.
 	*/
 	public void ingresarVacunas(String nombreVacuna, int cantidad, Fecha fechaIngreso) {
+		//"Pfizer" , "Moderna" , "Sinopharm" , "Astrazeneca" , "Sputnik"
+		if(!nombreVacuna.equals("Pfizer") && 
+				!nombreVacuna.equals("Moderna") &&
+				!nombreVacuna.equals("Sinopharm") &&
+				!nombreVacuna.equals("Astrazeneca") &&
+				!nombreVacuna.equals("Sputnik")) throw new RuntimeException("La vacuna no es valida");
+	
+		if(cantidad <=0) throw new RuntimeException("No es posible ingresar menos de una vacuna");
+		almacen.agregarVacunas(nombreVacuna, cantidad, fechaIngreso);
 	}
-	
-	
-	
-	
-	
+
 	/**
 	* total de vacunas disponibles no vencidas sin distinción por tipo.
 	*/
 	public int vacunasDisponibles() {
-		return 0;
+		return almacen.vacunasDisponibles();
 	}
 	
 	
@@ -56,7 +72,7 @@ public class CentroVacunacion {
 	* vacuna especificado.
 	*/
 	public int vacunasDisponibles(String nombreVacuna) {
-		return 0;
+		return almacen.vacunasDisponibles(nombreVacuna);
 	}
 	
 	
@@ -68,6 +84,19 @@ public class CentroVacunacion {
 	* Si la persona ya fue vacunada, también debe generar una excepción.
 	*/
 	public void inscribirPersona(int dni, Fecha nacimiento,boolean tienePadecimientos, boolean esEmpleadoSalud) {
+		Persona perso = new Persona(dni, nacimiento, esEmpleadoSalud, tienePadecimientos);
+		
+		//dni no valido
+		if(dni > 99999999 || dni<1000000) throw new RuntimeException("DNI no valido"); // TODO preguntar profe
+		//si es mayor de 18
+		if(perso.getEdad() <18) throw new RuntimeException("La persona debe ser mayor de 18 años");
+		//si no fue vacunada
+		if(this.reporteVacunacion.keySet().contains(perso.getDNI())) throw new RuntimeException("La persona ya fue vacunada");
+		//si no esta incripta
+		if(this.listaDeEspera.contains(perso)) throw new RuntimeException("La persona ya esta inscripta");
+		
+		//se agrega
+		this.listaDeEspera.add(perso);
 	}
 	
 	
@@ -79,7 +108,13 @@ public class CentroVacunacion {
 	* Si no quedan inscriptos sin vacunas debe devolver una lista vacía.
 	*/
 	public List<Integer> listaDeEspera() {
-		return null;
+		ArrayList<Integer> lista = new ArrayList<>();
+		
+		for (Persona perso : this.listaDeEspera) {
+			lista.add(perso.getDNI());
+		}
+		
+		return lista;
 	}
 	
 	
@@ -98,9 +133,67 @@ public class CentroVacunacion {
 	* el día del turno.
 	*/
 	public void generarTurnos(Fecha fechaInicial) {
+		
+		if(fechaInicial.compareTo(Fecha.hoy())<0) throw new RuntimeException("No se pueden generar turnos para fechas pasadas");
+		
+		
+		/********************primera parte*************************/
+		
+		//recorro todas las fechas
+		for (Fecha fecha : this.turnos.keySet()) {
+			//si la fecha esta vencida
+			if(fecha.compareTo(Fecha.hoy())<0) {
+				//itero sobre el arreglo de turnos
+				Iterator<Turno> it = this.turnos.get(fecha).iterator();
+				while(it.hasNext()) {
+						//se vuelve a amacenar la vacuna 
+						almacen.agregarVacuna(it.next().getVacuna());
+				}
+			}
+			this.turnos.remove(fecha);
+		}
+		/********************segunda parte*************************/
+		
+		almacen.moverVacunas();
+		
+		/********************tercera parte*************************/
+		ordenarListaDeEspera();
+		//si no hay turnos con esa fecha la define
+		if(!this.turnos.keySet().contains(fechaInicial)) {
+				this.turnos.put(fechaInicial, new ArrayList<Turno>());
+			}
+		
+		for (Persona persona : listaDeEspera) {
+			//si hay vacunas disponibles
+			
+			if(almacen.vacunasDisponibles()>0) {
+				
+				Vacuna aux = almacen.dameVacuna(persona.getEdad());
+				
+				if (aux != null) {
+					if(this.turnos.get(fechaInicial).size()<capacidadVacunacionDiaria) {
+						this.turnos.get(fechaInicial).add(new Turno(persona,aux , fechaInicial));
+						listaDeEspera.remove(persona);
+					}
+					else {
+						fechaInicial.avanzarUnDia();
+						this.turnos.put(fechaInicial, new ArrayList<Turno>());
+						this.turnos.get(fechaInicial).add(new Turno(persona, aux, fechaInicial));
+						listaDeEspera.remove(persona);
+					}
+				}
+			}
+			//ya no hay mas vacunas
+			break;
+		}
+		
+		
+		
+	} //fin del metodo
+	
+	private boolean fueVacunada(Persona perso) { //TODO ver si se usa 
+		return this.reporteVacunacion.keySet().contains(perso.getDNI());
 	}
-	
-	
 	
 	
 	
@@ -139,7 +232,7 @@ public class CentroVacunacion {
 	* - Y, el valor es el nombre de la vacuna aplicada.
 	*/
 	public Map<Integer, String> reporteVacunacion() {
-		return null;
+		return this.reporteVacunacion;
 	}
 	
 	
@@ -156,10 +249,43 @@ public class CentroVacunacion {
 		return almacen.reporteVacunasVencidas();
 	}
 	
-	
-	
-	
 	public void ordenarListaDeEspera() {
 		
 	}
+	
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("CentroVacunacion [nombreDelCentro=");
+		builder.append(nombreDelCentro);
+		builder.append(", capacidadVacunacionDiaria=");
+		builder.append(capacidadVacunacionDiaria);
+		builder.append(", almacen=");
+		builder.append(almacen);
+		builder.append(", lista de espera=");
+		builder.append(listaDeEspera);
+		builder.append("]");
+		return builder.toString();
+	}
+
+
+
+	
+	
+	public static void main(String[] args) {
+		CentroVacunacion ungs = new CentroVacunacion("UNGS", 10);
+		ungs.ingresarVacunas("Moderna", 20, Fecha.hoy());
+		ungs.ingresarVacunas("Pfizer", 15, Fecha.hoy());
+		ungs.inscribirPersona(65196953, new Fecha(14,06,1999), false, false);
+		ungs.inscribirPersona(68549616, new Fecha(14,06,1999), false, false);
+		System.out.println(ungs);
+		System.out.println(ungs.vacunasDisponibles());
+		System.out.println(ungs.vacunasDisponibles("Pfizer"));
+		System.out.println(ungs.listaDeEspera());
+
+		
+	}
+	
+	
 }
