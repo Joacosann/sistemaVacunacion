6853ca0c6a28324d5710b8628c8c6da026a2e57f
agust <agust@192.168.0.14>
2021-06-09 18:21:17 -0300

    removido el equals de persona

--- src/centroVacunacion/Persona.java
+++ src/centroVacunacion/Persona.java
@@ -70,36 +70,6 @@ public class Persona implements Comparable<Persona> {
 	public int getDNI() {
 		return DNI;
 	}
-
-	
-
-
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + DNI;
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Persona other = (Persona) obj;
-		if (DNI != other.DNI)
-			return false;
-		return true;
-	}
-
-
 	
 	@Override
 	public String toString() {
