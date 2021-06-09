6bb32e1f6cf4f0e9ca877c599d84716ed4880e7e
agust <agust@192.168.0.14>
2021-06-09 18:16:32 -0300

    Removido equals de vacuna

--- src/centroVacunacion/Moderna.java
+++ src/centroVacunacion/Moderna.java
@@ -27,12 +27,6 @@ public class Moderna extends Vacuna {
 	
 	//getters y setters
 	public Fecha getFechaVencimiento() {
-		StringBuilder fecha = new StringBuilder();
-		fecha.append(fechaVencimiento.dia());
-		fecha.append("/");
-		fecha.append(fechaVencimiento.mes());
-		fecha.append("/");
-		fecha.append(fechaVencimiento.anio());
 		return fechaVencimiento;
 	}
 
@@ -57,31 +51,6 @@ public class Moderna extends Vacuna {
 	}
 
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((fechaVencimiento == null) ? 0 : fechaVencimiento.hashCode());
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Moderna other = (Moderna) obj;
-		if (fechaVencimiento == null) {
-			if (other.fechaVencimiento != null)
-				return false;
-		} else if (!fechaVencimiento.equals(other.fechaVencimiento))
-			return false;
-		return true;
-	}
 
 
 
